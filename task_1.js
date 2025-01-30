05110ac7ab012467ba046181003589aa306cbc25
ARSmobi <102361458+ARSmobi@users.noreply.github.com>
2025-01-30 18:51:31 +0300

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -15,5 +15,25 @@ function evenOdd(arr){
     }
 }
 
+function evenOddCount(arr){
+    let even = 0;
+    let odd = 0;
+    let zero = 0;
+    for (let item of arr){
+        let num = +item;
+        if (typeof num == "number" && !isNaN(num)){
+            if (num === 0){
+                zero++;
+            } else if (num % 2 === 0){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+    }
+    console.log(`Четных: ${even}\nнечетных: ${odd}\nнулей: ${zero}`);
+}
+
 let arr = [1, 4, 'a', 0, 'b', 2, 0, 7, 'c', 5, 0, 9];
-evenOdd(arr);
+// evenOdd(arr);
+evenOddCount(arr);
